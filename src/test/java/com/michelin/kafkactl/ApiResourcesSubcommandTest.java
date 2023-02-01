afeef9723653dd6b1138fad7237c4d1edd75f463
Loïc Greffier <loic.greffier_ext@michelin.com>
2023-02-01 14:40:17 +0100

    Started unit tests

--- src/test/java/com/michelin/kafkactl/ApiResourcesSubcommandTest.java
+++ src/test/java/com/michelin/kafkactl/ApiResourcesSubcommandTest.java
@@ -47,17 +47,8 @@ class ApiResourcesSubcommandTest {
 
     @Test
     void shouldNotDisplayApiResourcesWhenNotAuthenticated() {
-        ApiResource apiResource = new ApiResource();
-        apiResource.setKind("Topic");
-        apiResource.setPath("topics");
-        apiResource.setNames(List.of("topics", "topic", "to"));
-        apiResource.setNamespaced(true);
-        apiResource.setSynchronizable(true);
-
         when(loginService.doAuthenticate())
                 .thenReturn(false);
-        when(apiResourcesService.getListResourceDefinition())
-                .thenReturn(Collections.singletonList(apiResource));
 
         int code = new CommandLine(apiResourcesSubcommand).execute();
         assertEquals(2, code);
