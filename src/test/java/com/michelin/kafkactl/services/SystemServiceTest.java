9be40f8f863963b7b162111e932b68723451ff61
Loïc Greffier <loic.greffier_ext@michelin.com>
2023-03-09 16:24:04 +0100

    Update unit tests set property

--- src/test/java/com/michelin/kafkactl/services/SystemServiceTest.java
+++ src/test/java/com/michelin/kafkactl/services/SystemServiceTest.java
@@ -3,10 +3,8 @@ package com.michelin.kafkactl.services;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SystemServiceTest {
-
     @Test
     void shouldGetEnv() {
         assertEquals(System.getenv("ANY_VAR_ENV"), SystemService.getEnv("ANY_VAR_ENV"));
@@ -15,5 +13,11 @@ class SystemServiceTest {
     @Test
     void shouldGetProperty() {
         assertEquals(System.getProperty("ANY_PROP"), SystemService.getProperty("ANY_PROP"));
+    }
+
+    @Test
+    void shouldSetProperty() {
+        SystemService.setProperty("MY_PROP", "VALUE");
+        assertEquals("VALUE", System.getProperty("MY_PROP"));
     }
 }
