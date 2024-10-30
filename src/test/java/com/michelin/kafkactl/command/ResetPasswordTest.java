036219238f16429e78cd574c952f921a40988306
thcai <thomas.cai_ext@michelin.com>
2024-10-30 12:12:47 +0100

    Expand import
    Signed-off-by: thcai <thomas.cai_ext@michelin.com>

--- src/test/java/com/michelin/kafkactl/command/ResetPasswordTest.java
+++ src/test/java/com/michelin/kafkactl/command/ResetPasswordTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.michelin.kafkactl.Kafkactl;
 import com.michelin.kafkactl.config.KafkactlConfig;
