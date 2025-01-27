e5a6f2c36db03306da84c44f2473ec3937f245aa
thcai <thomas.cai_ext@michelin.com>
2025-01-27 15:26:26 +0100

    unused import
    Signed-off-by: thcai <thomas.cai_ext@michelin.com>

--- src/main/java/com/michelin/kafkactl/service/ResourceService.java
+++ src/main/java/com/michelin/kafkactl/service/ResourceService.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.ParameterException;
