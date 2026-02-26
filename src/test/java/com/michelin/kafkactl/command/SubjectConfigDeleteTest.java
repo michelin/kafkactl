626857b38be1ce8b531a1037af3ba904085d972e
Thomas Cai <thomas.cai_ext@michelin.com>
2026-02-26 14:25:04 +0100

    Add the possibility to define subject alias

---
+++ src/test/java/com/michelin/kafkactl/command/SubjectConfigDeleteTest.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.michelin.kafkactl.command;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.when;
+
+import com.michelin.kafkactl.Kafkactl;
+import com.michelin.kafkactl.command.subjectconfig.SubjectConfigDelete;
+import com.michelin.kafkactl.model.Metadata;
+import com.michelin.kafkactl.model.Resource;
+import com.michelin.kafkactl.property.KafkactlProperties;
+import com.michelin.kafkactl.service.ConfigService;
+import com.michelin.kafkactl.service.LoginService;
+import com.michelin.kafkactl.service.ResourceService;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Collections;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import picocli.CommandLine;
+
+@ExtendWith(MockitoExtension.class)
+class SubjectConfigDeleteTest {
+    @Mock
+    LoginService loginService;
+
+    @Mock
+    ResourceService resourceService;
+
+    @Mock
+    ConfigService configService;
+
+    @Mock
+    KafkactlProperties kafkactlProperties;
+
+    @Mock
+    Kafkactl kafkactl;
+
+    @InjectMocks
+    SubjectConfigDelete subjectConfigDelete;
+
+    @Test
+    void shouldReturnInvalidCurrentContext() {
+        CommandLine cmd = new CommandLine(subjectConfigDelete);
+        StringWriter sw = new StringWriter();
+        cmd.setErr(new PrintWriter(sw));
+
+        when(configService.isCurrentContextValid()).thenReturn(false);
+
+        int code = cmd.execute("mySubject");
+        assertEquals(1, code);
+        assertTrue(sw.toString()
+                .contains("No valid current context found. "
+                        + "Use \"kafkactl config use-context\" to set a valid context."));
+    }
+
+    @Test
+    void shouldNotDeleteConfigWhenNotAuthenticated() {
+        when(configService.isCurrentContextValid()).thenReturn(true);
+        when(loginService.doAuthenticate(any(), anyBoolean())).thenReturn(false);
+
+        CommandLine cmd = new CommandLine(subjectConfigDelete);
+
+        int code = cmd.execute("mySubject");
+        assertEquals(1, code);
+    }
+
+    @Test
+    void shouldDeleteConfig() {
+        Resource resource = Resource.builder()
+                .kind("SubjectConfigState")
+                .apiVersion("v1")
+                .metadata(Metadata.builder()
+                        .name("mySubject")
+                        .namespace("namespace")
+                        .build())
+                .spec(Collections.emptyMap())
+                .build();
+
+        when(configService.isCurrentContextValid()).thenReturn(true);
+        when(loginService.doAuthenticate(any(), anyBoolean())).thenReturn(true);
+        when(resourceService.deleteSubjectConfig(any(), any(), any())).thenReturn(Optional.of(resource));
+
+        when(kafkactlProperties.getCurrentNamespace()).thenReturn("namespace");
+
+        CommandLine cmd = new CommandLine(subjectConfigDelete);
+
+        int code = cmd.execute("mySubject");
+        assertEquals(0, code);
+    }
+}
